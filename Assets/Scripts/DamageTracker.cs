afd9a2ec3b31117dff7b8c1d3a03875572d58c4f
g00197439 <60353410+g00197439@users.noreply.github.com>
2021-02-26 18:23:59 -0500

    Health System implemented
    It's very basic right now and will need changes

---
+++ Assets/Scripts/DamageTracker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageTracker : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public int damage;
+    public string[] tags;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        for (int i = 0; i < tags.Length; i++)
+        {
+            if (collision.gameObject.tag == tags[i])
+            {
+                collision.gameObject.GetComponent<HealthTracker>().Damage(damage);
+                return;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/HealthTracker.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthTracker : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public int health;
+
+    public void Damage(int damage)
+    {
+        health = health - damage;
+        if(health <= 0) this.gameObject.SetActive(false);
+    }
+}
