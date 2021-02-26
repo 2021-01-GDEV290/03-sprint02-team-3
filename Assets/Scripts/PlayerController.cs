db118ba8b2b62029052093430e1fc4263746b69d
g00197439 <60353410+g00197439@users.noreply.github.com>
2021-02-26 15:46:40 -0500

    PlayerController script and prefab created
    ...with a temporary sprite

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Set In Inspector")]
+    public float moveSpeed;
+
+    private float xInput;
+    private float yInput;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        xInput = Input.GetAxisRaw("Horizontal");
+        yInput = Input.GetAxisRaw("Vertical");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xInput = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        yInput = Input.GetAxisRaw("Vertical") * moveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector3(xInput, yInput, 0);
+    }
+}
