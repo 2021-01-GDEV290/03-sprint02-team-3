e9c80c14f5971dab2e6eae0059c4677e2bcfc1c6
g00197439 <60353410+g00197439@users.noreply.github.com>
2021-02-26 16:24:12 -0500

    Added assets from separate project
    Added:
    
    - Walls
    - Door
    - Door Script

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public bool collide = false;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            collide = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            collide = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (collide && Input.GetKeyDown(KeyCode.E))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
