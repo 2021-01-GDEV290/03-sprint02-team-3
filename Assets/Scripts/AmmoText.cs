847b9a3ed52fc30323404865cf075865e1c1b14a
g00197439 <60353410+g00197439@users.noreply.github.com>
2021-02-28 14:18:36 -0500

    Ammo count is now displayed on UI

---
+++ Assets/Scripts/AmmoText.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AmmoText : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public Text txt;
+    public GameObject player;
+
+    int currentAmmo;
+
+    private void Start()
+    {
+        currentAmmo = player.GetComponent<Shooting>().currentAmmo;
+    }
+
+    private void Update()
+    {
+        if (currentAmmo != player.GetComponent<Shooting>().currentAmmo)
+        {
+            currentAmmo = player.GetComponent<Shooting>().currentAmmo;
+            UpdateAmmo();
+        }
+    }
+
+    void UpdateAmmo()
+    {
+        txt.GetComponent<Text>();
+        txt.text = "Ammo: " + currentAmmo;
+    }
+}
