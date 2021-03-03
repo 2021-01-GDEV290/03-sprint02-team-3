fbc2cd78de24abca3f0048e74586926168154fc0
g00197439 <60353410+g00197439@users.noreply.github.com>
2021-03-03 12:22:23 -0500

    Weapon Upgrade complete and fixed zombie pathing issue
    The issue with the pathing was that when a new zombie spawned, it would not follow the player. This has been fixed.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public int damage = 1;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Zombie")
--- Assets/Scripts/Upgrade Scripts/WeaponUpgrade.cs
+++ Assets/Scripts/Upgrade Scripts/WeaponUpgrade.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class WeaponUpgrade : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int firstUpgradePrice;
+    public int secondUpgradePrice;
+    public int thirdUpgradePrice;
+    public int distance;
+
+    public bool canBeBought;
+    public int currentBuyableUpgrade;
+    GameObject player;
+    GameObject bullet;
+
+    private void Start()
     {
-        
+        player = GameObject.Find("Player");
+        bullet = GameObject.Find("Bullet");
+        canBeBought = false;
+        currentBuyableUpgrade = 1;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if ((player.transform.position - this.transform.position).magnitude < distance)
+        {
+            canBeBought = true;
+            if(Input.GetKeyDown(KeyCode.E)) {
+                if (currentBuyableUpgrade == 1 && PointsTracker.points >= firstUpgradePrice)
+                {
+                    PointsTracker.points -= firstUpgradePrice;
+                    currentBuyableUpgrade = 2;
+                    bullet.gameObject.GetComponent<DamageTracker>().damage = 2;
+                } else if(currentBuyableUpgrade == 2 && PointsTracker.points >= secondUpgradePrice)
+                {
+                    PointsTracker.points -= secondUpgradePrice;
+                    currentBuyableUpgrade = 3;
+                    player.GetComponent<Shooting>().maxAmmo = 20;
+                    player.GetComponent<Shooting>().currentAmmo = 20;
+                } else if(currentBuyableUpgrade == 3 && PointsTracker.points >= thirdUpgradePrice)
+                {
+                    PointsTracker.points -= firstUpgradePrice;
+                    currentBuyableUpgrade = 4;
+                    bullet.gameObject.GetComponent<DamageTracker>().damage = 3;
+                    player.GetComponent<Shooting>().maxAmmo = 30;
+                    player.GetComponent<Shooting>().currentAmmo = 30;
+                } else
+                {
+                    return;
+                }
+            }
+        }
+        else
+        {
+            canBeBought = false;
+        }
     }
 }
