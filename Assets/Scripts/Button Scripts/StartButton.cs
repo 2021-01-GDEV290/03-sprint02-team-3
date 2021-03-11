8a3263ae83911acc96a4a66f9652b951d424f80c
g00197439 <60353410+g00197439@users.noreply.github.com>
2021-03-11 11:30:54 -0500

    Basic title screen created with only one working button
    Options and quit buttons will both be working soon

---
+++ Assets/Scripts/Button Scripts/StartButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    public Animator trasition;
+    public float trasitionTime = 2f;
+    public void OnClick(string sceneName)
+    {
+        StartCoroutine(LoadLevel(sceneName));
+    }
+
+    IEnumerator LoadLevel(string sceneName)
+    {
+        trasition.SetTrigger("Start");
+        yield return new WaitForSeconds(trasitionTime);
+        SceneManager.LoadScene(sceneName);
+    }
+}
