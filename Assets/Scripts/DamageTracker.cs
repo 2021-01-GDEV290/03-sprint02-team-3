debc1ec5d9ac3f15fd3e11216f422678f414812b
g00197439 <60353410+g00197439@users.noreply.github.com>
2021-03-15 19:32:25 -0400

    Zombies now CORRECTLY damage player over time when colliding with them (still a little finicky though)

--- Assets/Scripts/DamageTracker.cs
+++ Assets/Scripts/DamageTracker.cs
@@ -8,19 +8,11 @@ public class DamageTracker : MonoBehaviour
     public int damage;
     public string[] tags;
 
-    bool runContinueDamage = false;
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (this.gameObject.tag == "Bullet")
         {
             Destroy(this.gameObject);
-        }
-        if(this.gameObject.tag == "Zombie")
-        {
-            runContinueDamage = true;
-            StartCoroutine(ContinueDamage(collision));
-            return;
         }
         for (int i = 0; i < tags.Length; i++)
         {
@@ -29,23 +21,6 @@ public class DamageTracker : MonoBehaviour
                 collision.gameObject.GetComponent<HealthTracker>().Damage(damage);
                 return;
             }
-        }
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if(collision.gameObject.tag == "Player")
-        {
-            runContinueDamage = false;
-        }
-    }
-
-    IEnumerator ContinueDamage(Collision2D collision)
-    {
-        while(runContinueDamage)
-        {
-            collision.gameObject.GetComponent<HealthTracker>().Damage(damage);
-            yield return new WaitForSeconds(1f);
         }
     }
 }
---
+++ Assets/Scripts/Zombie.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public int damage;
+    public string[] tags;
+
+    bool runContinueDamage = false;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Zombie collided with " + collision.gameObject.tag);
+        runContinueDamage = true;
+        StartCoroutine(ContinueDamage(collision));
+        return;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        Debug.Log("Zombie left collision with " + collision.gameObject.tag);
+        if (collision.gameObject.tag == "Player")
+        {
+            runContinueDamage = false;
+        }
+    }
+
+    IEnumerator ContinueDamage(Collision2D collision)
+    {
+        while (runContinueDamage)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                collision.gameObject.GetComponent<HealthTracker>().Damage(damage);
+            }
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
