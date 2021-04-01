f26bff1fc5e6876551d3d1c3e256153742581a9c
g00197439 <60353410+g00197439@users.noreply.github.com>
2021-04-01 12:07:37 -0400

    Implementation of weapon drop system part 2
    - Animations set up with all weapon types (minigun looks a little weird)
    - All stats set up for all weapon types

--- Assets/Scripts/WeaponDrop.cs
+++ Assets/Scripts/WeaponDrop.cs
@@ -16,14 +16,17 @@ public class WeaponDrop : MonoBehaviour
                 case "Assault Rifle":
                     player.state = PlayerState.assaultRifle;
                     player.animator.SetInteger("Current State", 1);
+                    player.currentAmmo = player.maxAmmo[1];
                     break;
                 case "Shotgun":
                     player.state = PlayerState.shotgun;
                     player.animator.SetInteger("Current State", 2);
+                    player.currentAmmo = player.maxAmmo[2];
                     break;
                 case "Minigun":
                     player.state = PlayerState.minigun;
                     player.animator.SetInteger("Current State", 3);
+                    player.currentAmmo = player.maxAmmo[3];
                     break;
             }
             Destroy(gameObject);
