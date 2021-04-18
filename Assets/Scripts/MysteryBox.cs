fd31535f5ec7baeb6734b0a05d782536708baa00
g00197439 <60353410+g00197439@users.noreply.github.com>
2021-04-18 16:54:42 -0400

    Added a new "upgrade" that gives the player a random weapon

---
+++ Assets/Scripts/MysteryBox.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MysteryBox : MonoBehaviour
+{
+    public int pointsNeeded;
+    public float distance;
+    public float ARChance;
+    float tempARChance;
+    public float shotgunChance;
+    float tempShotgunChance;
+    public float minigunChance;
+    public bool canBeBought;
+
+    GameObject player;
+    Player playerScript;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+        playerScript = player.GetComponent<Player>();
+        canBeBought = false;
+        tempARChance = ARChance;
+        tempShotgunChance = tempARChance + shotgunChance;
+    }
+
+    private void Update()
+    {
+        if (player == null)
+        {
+            return;
+        }
+        if ((player.transform.position - transform.position).magnitude < distance)
+        {
+            canBeBought = true;
+            if (Input.GetKeyDown(KeyCode.E) && Player.points >= pointsNeeded)
+            {
+                float thisRandom = Random.Range(0, ARChance + shotgunChance + minigunChance);
+                if (thisRandom <= tempARChance)
+                {
+                    playerScript.state = PlayerState.assaultRifle;
+                    playerScript.animator.SetInteger("Current State", 1);
+                    playerScript.currentAmmo = playerScript.maxAmmo[1];
+                    playerScript.remainingAmmo = playerScript.maxAmmo[1] * 2;
+                }
+                else if (thisRandom <= tempShotgunChance)
+                {
+                    playerScript.state = PlayerState.shotgun;
+                    playerScript.animator.SetInteger("Current State", 2);
+                    playerScript.currentAmmo = playerScript.maxAmmo[2];
+                    playerScript.remainingAmmo = playerScript.maxAmmo[2] * 3;
+                }
+                else
+                {
+                    playerScript.state = PlayerState.minigun;
+                    playerScript.animator.SetInteger("Current State", 3);
+                    playerScript.currentAmmo = playerScript.maxAmmo[3];
+                }
+            }
+        }
+        else
+        {
+            canBeBought = false;
+        }
+    }
+}
---
+++ Assets/Scripts/MysteryBoxText.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MysteryBoxText : MonoBehaviour
+{
+    public GameObject mysteryBox;
+    int pointsNeeded;
+
+    private void Start()
+    {
+        pointsNeeded = mysteryBox.gameObject.GetComponent<MysteryBox>().pointsNeeded;
+    }
+
+    private void Update()
+    {
+        if (mysteryBox.GetComponent<MysteryBox>().canBeBought)
+        {
+            GetComponent<Text>().text = "PRESS E TO \nOBTAIN A RANDOM WEAPON\n(" + pointsNeeded + " POINTS)";
+        }
+        else
+        {
+            GetComponent<Text>().text = "";
+        }
+    }
+}
