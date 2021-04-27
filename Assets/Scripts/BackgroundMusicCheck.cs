535fffec2fd9e7f2fcc0462a59f52eb3fce38a04
g00197439 <60353410+g00197439@users.noreply.github.com>
2021-04-27 18:11:12 -0400

    Fixed an issue with the Audio Mixers only being set to the volume in PlayerPrefs when clicking into the Settings menu, not when loading the game

--- Assets/Scripts/BackgroundMusicCheck.cs
+++ Assets/Scripts/BackgroundMusicCheck.cs
@@ -13,5 +13,7 @@ public class BackgroundMusicCheck : MonoBehaviour
         {
             Destroy(musicGOs[i]);
         }
+        
+        
     }
 }
---
+++ Assets/Scripts/GameStart.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class GameStart : MonoBehaviour
+{
+    public AudioMixer[] audioMixers;
+
+    void Start()
+    {
+        audioMixers[0].SetFloat("volume", Mathf.Log10(PlayerPrefs.GetFloat("MusicVolume")) * 20);
+        audioMixers[1].SetFloat("volume", Mathf.Log10(PlayerPrefs.GetFloat("SoundVolume")) * 20);
+        enabled = false;
+    }
+}
