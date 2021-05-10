48524bdbb38e3dde980a1c3b71ef9c1df458f7c4
g00197439 <60353410+g00197439@users.noreply.github.com>
2021-05-10 13:33:20 -0400

    UI updated to show the player which upgrades they have and at which tiers

---
+++ Assets/Scripts/Upgrade Scripts/HealthUpgradeUI.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUpgradeUI : MonoBehaviour
+{
+    public HealthUpgrade script;
+
+    public GameObject upgradeImage;
+
+    Text txt;
+
+    private void Start()
+    {
+        txt = GetComponent<Text>();
+        upgradeImage.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (script.currentBuyableUpgrade == 1)
+        {
+            txt.text = "";
+        }
+        else if (script.currentBuyableUpgrade == 2)
+        {
+            upgradeImage.SetActive(true);
+            txt.text = "I";
+        }
+        else if (script.currentBuyableUpgrade == 3)
+        {
+            txt.text = "II";
+        }
+        else if (script.currentBuyableUpgrade == 4)
+        {
+            txt.text = "III";
+        }
+    }
+}
---
+++ Assets/Scripts/Upgrade Scripts/WeaponUpgradeUI.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponUpgradeUI : MonoBehaviour
+{
+    public WeaponUpgrade script;
+
+    public GameObject upgradeImage;
+
+    Text txt;
+
+    private void Start()
+    {
+        txt = GetComponent<Text>();
+        upgradeImage.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (script.currentBuyableUpgrade == 1)
+        {
+            txt.text = "";
+        }
+        else if (script.currentBuyableUpgrade == 2)
+        {
+            upgradeImage.SetActive(true);
+            txt.text = "I";
+        }
+        else if (script.currentBuyableUpgrade == 3)
+        {
+            txt.text = "II";
+        }
+        else if (script.currentBuyableUpgrade == 4)
+        {
+            txt.text = "III";
+        }
+        else if (script.currentBuyableUpgrade > 4)
+        {
+            txt.text = "IV+";
+        }
+    }
+}
