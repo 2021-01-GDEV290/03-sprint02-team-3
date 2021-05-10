de19699d07c568f97b69aa6d799f3813a96c2dea
g00197439 <60353410+g00197439@users.noreply.github.com>
2021-05-10 14:33:24 -0400

    Fixed a bug that only allowed for the normal zombies to be damaged

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -8,7 +8,8 @@ public class Bullet : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Zombie")
+        if(collision.gameObject.tag == "Zombie" || collision.gameObject.tag == "Fast Zombie" ||
+            collision.gameObject.tag == "Ranged Zombie" || collision.gameObject.tag == "Boss Zombie")
         {
             collision.gameObject.GetComponent<Zombie>().Damage(damage);
         }
